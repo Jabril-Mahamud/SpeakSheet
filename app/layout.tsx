b534c45fe5cb7f236fa40f35505098240b22ce61
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2025-02-27 10:59:58 +0000

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "SpeakSheet",
-  description: "The fastest way to create & manage Text to speech audio content",
+  description: "The best way to create & manage Text to speech audio content",
 };
 
 export default function RootLayout({
