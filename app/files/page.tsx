892c1b65837031e1d5e0c62042f7e46429b875d5
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2025-04-30 14:24:25 +0100

    alignment

--- app/files/page.tsx
+++ app/files/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Library = () => {
   return (<>
     <div>Library Page</div>
-    <h1>Sorry, I'm reworking my entire app. I'm cannibalising this for now.</h1>
+    <h1 className='text-center'>Sorry, I'm reworking my entire app. I'm cannibalising this for now.</h1>
     </>
   )
 }
