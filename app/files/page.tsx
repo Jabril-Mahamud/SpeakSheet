c04a1b7087185129890bb47a872ee6a8763cf1b0
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2025-04-30 14:23:46 +0100

    added files page

---
+++ app/files/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Library = () => {
+  return (<>
+    <div>Library Page</div>
+    <h1>Sorry, I'm reworking my entire app. I'm cannibalising this for now.</h1>
+    </>
+  )
+}
+
+export default Library
